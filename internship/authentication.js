5e0be43cbb6dcd23e0d916c32090478e6909e049
LCRinesnow <602284158@qq.com>
2016-05-15 18:35:56 +0800

    OAthen2

--- internship/authentication.js
+++ internship/authentication.js
@@ -2,7 +2,25 @@
  * Created by rinesnow on 16/3/31.
  */
 //未登录
+
+
 function ifAuthorized(req,res,next){
+   var OAuth = require('wechat-oauth');
+   var client = new OAuth('wx75d11b4f981b1ded', '7a915c525f39451f3af19407012459ad');
+
+    var query = require('url').parse(req.url,true).query;
+          console.log(query);
+
+    var code = query.code;
+          console.log(code);
+
+    client.getAccessToken(code, function (err, result) {
+      var accessToken = result.data.access_token;
+      var openid = result.data.openid;
+      console.log(access_token);
+      console.log(openid);
+
+    });
     if(!req.session.user){
         console.log('抱歉,您还没有登录!');
         return res.redirect('/login');//返回登录页面
