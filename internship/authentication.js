064c9006182cc12c78199186683dabc593dea10e
LCRinesnow <602284158@qq.com>
2016-06-07 12:17:56 +0800

    yonghu id

--- internship/authentication.js
+++ internship/authentication.js
@@ -25,7 +25,7 @@ function ifAuthorized(req,res,next){
     var code = query.code;
           console.log(code);
 
-    client.getAccessToken(code, function (err, result) {
+    client.getAccessToken('code', function (err, result) {
       var accessToken = result.data.access_token;
       var openid = result.data.openid;
       console.log(accessToken);
