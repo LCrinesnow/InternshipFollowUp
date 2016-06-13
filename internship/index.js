3183324e95a190cc5a541af3e4c09e2822b9046f
LCRinesnow <602284158@qq.com>
2016-06-13 12:53:06 +0800

    enterprise

--- internship/index.js
+++ internship/index.js
@@ -80,7 +80,6 @@ app.use(function(req, res, next) {
 });
 //添加  页面提示功能   目前没用 
 
-
 var OAuth = require('wechat-oauth');
 var client = new OAuth('wx1d3765eb45497a18', 'ZMrYxJD9UWZyM5oG2lsDMIuBUpirLBLDXX__2LvSF-WGYuGzAtIcBe-f1_SF1Giw');
 app.get('/',function ifAuthorized(req,res){
@@ -110,8 +109,8 @@ app.get('/',function ifAuthorized(req,res){
         console.log("else"+code);
         client.getUserByCode(code,function (err, result) {
       // var openid = result.openid;//必须要手动点击URL，原地刷新没用的。
-            console.log('这是result:'+result.openid);
-            req.session.openid=result.openid;
+            console.log('这是result:'+result.userid);
+            req.session.openid=result.userid;
 
             User.findOne({openid:req.session.openid},function(err,user){
                if(err){
